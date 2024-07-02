d4ffa1fe0313e267d3359dd8092c4aeaeb2f0e2d
atarwn <102351430+atarwn@users.noreply.github.com>
2024-07-02 20:40:16 +0500

    Delete wfcms0 directory

--- wfcms0/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$adminPassword = 'your_password'; // Задайте пароль администратора
-$rootDir = $_SERVER['DOCUMENT_ROOT']; // Корневой каталог сайта
-$overwriteFromTemplate = true; // Перезаписывать ли файлы из шаблона
-$enableNicedit = true; // Включить ли графический WYSIWYG редактор NicEdit
-?>
